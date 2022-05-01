e6a72502961729d4673f5a7a9c31a5fc957b7706
weiowo <97839979+weiowo@users.noreply.github.com>
2022-05-01 14:58:24 +0800

    Add files via upload

---
+++ WebComponents/header2.js
@@ -0,0 +1,158 @@
+
+class headerTemplate extends HTMLElement{
+    constructor(){
+        super();
+        this.setAttribute("class","header");
+        this.applyRoutes();
+        this.applyAction();
+        //想問下面這兩行～～～
+        // const shadowRoot = this.attachShadow({mode: 'open'});
+        // shadowRoot.appendChild(headerTemplate.content.cloneNode(true));   
+    }
+
+//創造Routes部分-->內含 beforeclicked(indludes logo,smallbar)+navbar
+
+    applyRoutes(){
+        let route = document.createElement("div");
+        route.setAttribute("class","routes");
+        this.appendChild(route);
+        let beforeClicked = document.createElement("div");
+        beforeClicked.setAttribute("class", "before-clicked");
+        route.appendChild(beforeClicked);
+        let logo = document.createElement("div");
+        logo.setAttribute("class","logo");
+        logo.innerHTML = `<img src="images/logo.png" alt="logo">`
+        beforeClicked.appendChild(logo);
+        let smallbar = document.createElement("form");
+        smallbar.setAttribute("class", "small-search-bar");
+        smallbar.innerHTML=`
+        <input class="small-search-bar-input" type="text">
+        <button class="small-search-bar-button" type="submit">
+            <img class="small-search-bar-img" src="images/search.png">
+        </button>`
+        beforeClicked.appendChild(smallbar);
+
+        let navbar = document.createElement("div");
+        navbar.setAttribute("class", "navbar")
+        navbar.innerHTML = `
+        <span class="nav-text" id="colored-text">
+            女裝
+            </span>
+            <span class="nav-text"  id="nav-text-middle-bar">
+             |
+            </span>
+            <span class="nav-text">
+            男裝
+            </span>
+            <span class="nav-text"  id="nav-text-middle-bar">
+             |
+            </span>
+            <span class="nav-text">
+            配件
+            </span> `
+        route.appendChild(navbar);
+    }
+    //創造 action部分=searchbar+icon-cart+icon-member
+
+    applyAction(){
+        let actions = document.createElement("div");
+        actions.setAttribute("class","actions");
+        this.appendChild(actions);
+
+        let searchBar = document.createElement("form");
+        searchBar.setAttribute("class","search-bar");
+        searchBar.innerHTML=`<input type="text" class="data-search">
+        <button type="submit"><img src="images/search.png"></button>`;
+        actions.appendChild(searchBar);
+
+        let iconCart=document.createElement("div");
+        iconCart.setAttribute("class","icon-cart");
+        iconCart.innerHTML=
+        `<a href="#"><img src="images/cart.png" class="shopping-cart"></a>`;
+        actions.appendChild(iconCart);
+
+        let iconMember=document.createElement("div");
+        iconMember.setAttribute("class","icon-member");
+        iconMember.innerHTML=`<a href="#"><img src="images/member.png" class="member"></a>`;
+        actions.appendChild(iconMember);
+    }
+}
+
+//custom element客製化tag
+customElements.define("header-test2", headerTemplate);
+
+
+
+
+
+
+
+
+
+
+// const headerTemplate = document.createElement('template');
+// headerTemplate.innerHTML = `
+
+// <style src="style.css"></style>
+// <header class="header">
+//     <div class="routes">
+//         <div class="before-clicked">
+//             <div class="logo" onclick="directToHome()">
+//                 <img src="images/logo.png" alt="logo">
+//             </div> 
+//             <form class="small-search-bar">
+//                 <input class="small-search-bar-input" type="text">
+//                 <button class="small-search-bar-button" type="submit">
+//                     <img class="small-search-bar-img" src="images/search.png">
+//                 </button>
+//             </form>
+//         </div>
+//         <div class="navbar">
+//             <span class="nav-text" id="colored-text">
+//             女裝
+//             </span>
+//             <span class="nav-text"  id="nav-text-middle-bar">
+//              |
+//             </span>
+//             <span class="nav-text">
+//             男裝
+//             </span>
+//             <span class="nav-text"  id="nav-text-middle-bar">
+//              |
+//             </span>
+//             <span class="nav-text">
+//             配件
+//             </span> 
+//         </div>
+//     </div>
+//     <div class="actions">
+
+//         <form action="" class="search-bar">
+//             <input type="text" class="data-search">
+//             <button type="submit"><img src="images/search.png"></button>
+//         </form>
+
+//         <div class="icon-cart">
+//             <a href="#"><img src="images/cart.png" class="shopping-cart"></a>
+//         </div>
+
+//         <div class="icon-member">
+//             <a href="#"><img src="images/member.png" class="member"></a>
+//         </div>
+//     </div>
+
+// </header>
+// `
+
+// class Header extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
+
+//     connectedCallback() {
+//         const shadowRoot = this.attachShadow({ mode: 'open' });
+//         shadowRoot.appendChild(headerTemplate.content);
+//     }
+// }
+
+// customElements.define('header-component', Header);
